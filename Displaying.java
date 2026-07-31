1cb1161c950ec4fc3a0049ae25a6762616f819bd
Harshita Mehta <mharshita687@gmail.com>
2026-07-31 22:33:53 +0530

    Add files via upload

---
+++ Displaying.java
@@ -0,0 +1,14 @@
+public class Displaying {
+    public static void main(String[] args) {
+        System.out.println("screen display");
+        for (int i = 0; i <= 9; i++){
+            for (int j = 1; j <= i; j++)
+                {
+                    System.out.println(" ");
+                    System.out.println(i);
+                }
+                System.out.println("\n");
+        }
+        System.out.println("screen display is done");
+    }
+}
